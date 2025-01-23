c36139c09d26b5a883162cb83437f6f4c10b2757
PeterKassenaar <info@kassenaar.com>
2025-01-23 10:48:34 +0100

    feat: add example 120-basic-navigation

---
+++ examples/120-basic-navigation/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <MainNavigation/>
+    <slot/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import MainNavigation from "@/components/MainNavigation.vue";
+</script>
---
+++ examples/120-basic-navigation/pages/history.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>History of C#</h2>
+    <div>
+      In January 1999, Anders Hejlsberg formed a team to build a new language at the time called Cool, which stood for
+      "C-like Object Oriented Language".[24] Microsoft had considered keeping the name "Cool" as the final name of the
+      language, but chose not to do so for trademark reasons. By the time the .NET project was publicly announced at the
+      July 2000 Professional Developers Conference, the language had been renamed C#, and the class libraries and
+      ASP.NET
+      runtime had been ported to C#.
+    </div>
+    <div>
+      Source: <a href="https://en.wikipedia.org/wiki/C_Sharp_(programming_language)">
+      https://en.wikipedia.org/wiki/C_Sharp_(programming_language)
+    </a>
+    </div>
+</template>
+<script setup lang="ts">
+</script>
