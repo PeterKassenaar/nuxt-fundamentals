f603d7d3b771ccf62991c61f4663c487b3bac123
PeterKassenaar <info@kassenaar.com>
2025-01-31 11:22:43 +0100

    feat: add example 180-server-routes

---
+++ examples/180-server-routes/pages/history.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2 class="text-2xl font-bold">History of C#</h2>
+  <div class="text-base">
+    In January 1999, Anders Hejlsberg formed a team to build a new language at the time called Cool, which stood for
+    "C-like Object Oriented Language".[24] Microsoft had considered keeping the name "Cool" as the final name of the
+    language, but chose not to do so for trademark reasons. By the time the .NET project was publicly announced at the
+    July 2000 Professional Developers Conference, the language had been renamed C#, and the class libraries and
+    ASP.NET
+    runtime had been ported to C#.
+  </div>
+  <div class="text-base">
+    Source: <a href="https://en.wikipedia.org/wiki/C_Sharp_(programming_language)">
+    https://en.wikipedia.org/wiki/C_Sharp_(programming_language)
+  </a>
+  </div>
+</template>
