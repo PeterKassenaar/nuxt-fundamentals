40cd0758558c2cd25d5e0f12ffd4a440d4842a35
PeterKassenaar <info@kassenaar.com>
2025-02-17 13:12:36 +0100

    chore: some text updates in 180-server-routes

--- examples/180-server-routes/server/api/cities.ts
+++ examples/180-server-routes/server/api/cities.ts
@@ -1,3 +1,5 @@
+// cities.ts. Example of returning some data from a server side route.
+// This route is calles via '/api/cities' and retrieves the <string[]> of cities.
 export default defineEventHandler(() => {
     return [
         'Berlin', 'Amsterdam', 'Paris', 'London'
